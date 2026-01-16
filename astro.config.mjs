51b09d34aea7d35a6a5c2f4e21873eeef151abf5
Czer0C <trieunguyen04239@gmail.com>
2026-01-16 17:07:47 +0000

    chore: more image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import react from '@astrojs/react';
 
@@ -15,4 +15,7 @@ export default defineConfig({
 
   integrations: [react()],
   adapter: cloudflare(),
+  image: {
+    service: passthroughImageService()
+  }
 });
